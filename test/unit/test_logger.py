ec86e253e5fdaca0c2eb82d17d9ac6a96298dda0
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-11-14 12:16:46 -0700

    chore: refactor code into smaller functions, add more tests

--- test/unit/test_logger.py
+++ test/unit/test_logger.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import tempfile
 from unittest.mock import mock_open, patch
 
 import pytest
@@ -25,22 +26,43 @@ def test_setup(mock_make_dirs):
 
     mock_make_dirs.assert_called_once()
     assert logging.getLevelName(logger.getEffectiveLevel()) == 'INFO'
+
+    # The following get set from the file handler but are tested here since we have the
+    # complete instance returned from this function.
     assert logger.handlers[1].formatter._fmt == '%(asctime)s - %(levelname)s - %(message)s'
     assert 'test/mock-dir/test.log' in logger.handlers[1].baseFilename
     assert woodchips.logger.DEFAULT_LOG_MAX_BYTES == logger.handlers[1].maxBytes
     assert woodchips.logger.DEFAULT_LOG_BACKUP_COUNT == logger.handlers[1].backupCount
 
 
-@patch('os.makedirs')
-def test_setup_bad_log_level(mock_make_dirs):
+def test_setup_console_handler():
+    """Test that the console handler gets setup without breaking."""
+    woodchips.logger._setup_console_handler(logging.getLogger(__name__))
+
+
+@pytest.mark.parametrize(
+    'log_level, expected_log_value',
+    [
+        ('CRITICAL', 50),
+        ('ERROr', 40),
+        ('WARNING', 30),
+        ('INFO', 20),
+        ('DEBUG', 10),
+        ('NOTSET', 0),
+    ],
+)
+def test_validate_log_level(log_level, expected_log_value):
+    """Tests that we map log levels correctly."""
+    active_log_level = woodchips.logger._validate_log_level(log_level)
+
+    assert active_log_level == expected_log_value
+
+
+def test_validate_log_level_invalid():
     """Test that we raise an error when a bad log level is passed in."""
     with patch('builtins.open', mock_open()):
         with pytest.raises(KeyError) as error:
-            _ = woodchips.setup(
-                logger_name=__name__,
-                log_location=MOCK_LOG_PATH,
-                log_level='BAD_LOG_LEVEL',
-            )
+            woodchips.logger._validate_log_level('BAD_LOG_LEVEL')
 
     assert (
         str(error.value)
@@ -50,15 +72,15 @@ def test_setup_bad_log_level(mock_make_dirs):
     )
 
 
-@patch('os.makedirs')
-@patch('woodchips.logger')
-def test_setup_logging_dir_exists(mock_logger, mock_make_dirs):
-    # TODO: Mock this better so we don't need a gitignored empty folder for testing
-    if not os.path.exists(LOG_PATH_EXISTS):
-        os.mkdir(LOG_PATH_EXISTS)
+def test_setup_file_handler_dir_does_not_exist():
+    """Tests that we create the log path correctly.
 
-    _ = woodchips.setup(
-        logger_name=__name__,
-        log_location=LOG_PATH_EXISTS,
-        log_level=LOG_LEVEL,
-    )
+    NOTE: Other tests related to this function are tested in the `setup` tests.
+    """
+    with tempfile.TemporaryDirectory() as temp_dir:
+        woodchips.logger._setup_file_handler(
+            logger_instance=logging.getLogger(__name__),
+            log_location=temp_dir,
+        )
+
+        assert os.path.exists(temp_dir)
--- woodchips/logger.py
+++ woodchips/logger.py
@@ -12,13 +12,46 @@ def setup(logger_name: str, log_location: str, log_level: str = 'INFO') -> loggi
     """Setup project logging based on configuration.
 
     Each module that requires logging should instantiate this function and pass a
-    new name based on the module using logging, which for most purposes should be
-    `__name__` passed as `logger_name`.
+    new name based on the module using logging. Typically for most purposes, this
+    should be `__name__` passed as `logger_name`.
 
     TODO: Allow the user to specify if they want console/file logging or not.
     """
     # Create the logger
     logger_instance = logging.getLogger(logger_name)
+    active_log_level = _validate_log_level(log_level)
+    logger_instance.setLevel(active_log_level)
+
+    # Setup logging handlers
+    _setup_console_handler(logger_instance)
+    _setup_file_handler(logger_instance, log_location)
+
+    return logger_instance
+
+
+def _setup_console_handler(logger_instance: logging.Logger) -> None:
+    console_handler = logging.StreamHandler()
+    logger_instance.addHandler(console_handler)
+
+
+def _setup_file_handler(logger_instance: logging.Logger, log_location: str) -> None:
+    if not os.path.exists(log_location):
+        os.makedirs(log_location)
+
+    file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    # Splitting on the period assuming the user specified `__name__` so we can get the package name
+    log_name = logger_instance.name.split('.')[0] + '.log'
+    log_file = os.path.join(log_location, log_name)
+    file_handler = logging.handlers.RotatingFileHandler(
+        log_file,
+        maxBytes=DEFAULT_LOG_MAX_BYTES,
+        backupCount=DEFAULT_LOG_BACKUP_COUNT,
+    )
+    file_handler.setFormatter(file_formatter)
+    logger_instance.addHandler(file_handler)
+
+
+def _validate_log_level(log_level: str = 'INFO') -> str:
     log_levels = {
         'CRITICAL': logging.CRITICAL,  # 50
         'ERROR': logging.ERROR,  # 40
@@ -29,32 +62,10 @@ def setup(logger_name: str, log_location: str, log_level: str = 'INFO') -> loggi
     }
 
     try:
-        selected_log_level = log_levels[log_level.upper()]
+        active_log_level = log_levels[log_level.upper()]
     except KeyError as error:
         raise KeyError(
             f'Could not setup Woodchips due to invalid log level: {error}, must be one of {log_levels.keys()}'  # noqa
         )
 
-    logger_instance.setLevel(selected_log_level)
-
-    # Setup console handling
-    console_handler = logging.StreamHandler()
-    logger_instance.addHandler(console_handler)
-
-    # Setup file handling
-    if not os.path.exists(log_location):
-        os.makedirs(log_location)
-
-    file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-    # Splitting on the period assuming the user specified `__name__`` so we can get the package name
-    log_name = logger_name.split('.')[0] + '.log'
-    log_file = os.path.join(log_location, log_name)
-    file_handler = logging.handlers.RotatingFileHandler(
-        log_file,
-        maxBytes=DEFAULT_LOG_MAX_BYTES,
-        backupCount=DEFAULT_LOG_BACKUP_COUNT,
-    )
-    file_handler.setFormatter(file_formatter)
-    logger_instance.addHandler(file_handler)
-
-    return logger_instance
+    return active_log_level
